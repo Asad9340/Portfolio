1426073edf0c3721a9fd6b7897a3c4f6be2c916b
Asad9340 <asadulimran1999@gmail.com>
2024-02-24 22:55:19 +0600

    added new header or navbar section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{html,js}',
-    'node_modules/preline/dist/*.js',
-  ],
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('preline/plugin'),
-  ],
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 };
